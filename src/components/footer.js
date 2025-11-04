9a00917ba7a0008badfa3ffb678f985f9817b574
asnasharin28 <145577303+asnasharin28@users.noreply.github.com>
2025-11-04 17:28:42 +0530

    Removed unused FaTwitter import to fix Netlify build error

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export default function Footer() {
   return (
